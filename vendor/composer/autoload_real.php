a5bf40e19deaad60a0fc95b26506665e1efeaa47
Pavol Senko <pavol.senko@gmail.com>
2018-08-26 17:32:54 +0200

    generate autoloaders

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit00970101ecfc8385c42e7c0e5acc6350
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit00970101ecfc8385c42e7c0e5acc6350', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
